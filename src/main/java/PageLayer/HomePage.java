ebce4423ba9a27519520e2934eebe908905465ed
asbho <asbhosale8@gmail.com>
2025-07-05 22:16:19 +0530

    home page created

---
+++ src/main/java/PageLayer/HomePage.java
@@ -0,0 +1,10 @@
+package PageLayer;
+
+public class HomePage {
+	
+	public void home()
+	{
+		System.out.println("Home page");
+	}
+
+}
--- src/test/java/TestLayer/LoginTest.java
+++ src/test/java/TestLayer/LoginTest.java
@@ -1,5 +1,6 @@
 package TestLayer;
 
+import PageLayer.HomePage;
 import PageLayer.LoginPage;
 
 public class LoginTest extends LoginPage{
@@ -9,6 +10,9 @@ public class LoginTest extends LoginPage{
 		LoginPage log=new LoginPage();
 	     log.loginpage();
 		
+	     HomePage ho=new HomePage();
+	     ho.home();
+	     
 
 	}
 
